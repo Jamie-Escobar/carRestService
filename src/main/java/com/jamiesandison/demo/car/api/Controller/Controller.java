00d8158ccec2f787d0441f28171a6d48e7c84cb1
Jamie Sandison <jamie.sandison@sky.uk>
2022-12-09 14:34:50 +0000

    deleted original endpoint

--- src/main/java/com/jamiesandison/demo/car/api/Controller/Controller.java
+++ src/main/java/com/jamiesandison/demo/car/api/Controller/Controller.java
@@ -3,15 +3,17 @@ package com.jamiesandison.demo.car.api.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/private")
 public class Controller {
 
-    @GetMapping("statusResponse")
-    public HttpStatus getResponse() {
+    @GetMapping("/status")
+    public ResponseEntity<String> response() {
 
-        return HttpStatus.OK;
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 
 
--- src/test/java/com/jamiesandison/demo/car/api/DemoCarApiApplicationTests.java
+++ src/test/java/com/jamiesandison/demo/car/api/DemoCarApiApplicationTests.java
@@ -4,17 +4,28 @@ import com.jamiesandison.demo.car.api.Controller.Controller;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class DemoCarApiApplicationTests {
 
 	@Test
-	void httpResponse_200() {
+	void statusResponse_httpResponse_200() {
 
 		Controller controller = new Controller();
 		String response = String.valueOf(controller.getResponse());
 
 		Assertions.assertEquals("200 OK", response);
+	}
+
+	@Test
+	public void responseEntityTest() {
+
+		Controller controller = new Controller();
+		ResponseEntity<String> response = controller.response();
+
+		Assertions.assertEquals("200", response);
+
 	}
 
 }
