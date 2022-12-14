c04cb602d1b7a95356a57bce9f53511621326d5b
Jamie Sandison <jamie.sandison@sky.uk>
2022-12-14 09:37:24 +0000

    Functional test: refactored build

--- functional/src/test/java/steps/StepDefinitions.java
+++ functional/src/test/java/steps/StepDefinitions.java
@@ -2,8 +2,6 @@ package steps;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
